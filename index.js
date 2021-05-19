c9c9dab6c1f180b57b7befcfcf0dac53e2a135a8
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-19 04:06:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -226,10 +226,10 @@ function makeRender(gl, colorUniformLocation) {
 }
 
 function getBasis(unitsPerAxe) {
-  const semiAxisUnits = unitsPerAxe / 4;
+  const semiAxisUnits = unitsPerAxe / 2;
   const axes = {
     primitive: WebGLRenderingContext.LINES,
-    color: [0.5, 0.5, 0.5, 1],
+    color: [0.5, 0.1, 0.5, 1],
     components: [
       -semiAxisUnits, 0,
       semiAxisUnits, 0,
