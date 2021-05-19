c3d9aed9bfc66cd2714ce4b61c5f9beab2932722
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-19 04:33:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
 function makeRender(gl, colorUniformLocation) {
   return (objects)  => {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(1, 0, 1, 1);
+    gl.clearColor(1, 1, 1, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     objects.forEach(({ components, primitive, color }) => {
