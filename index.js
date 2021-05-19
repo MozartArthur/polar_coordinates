b03da49754ecd16fb9342aeb408d9307e61d5803
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-19 03:26:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -206,6 +206,7 @@ function createProgram(gl, vertexShader, fragmentShader) {
 function makeRender(gl, colorUniformLocation) {
   return (objects)  => {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(1, 1, 1, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     objects.forEach(({ components, primitive, color }) => {
@@ -228,6 +229,7 @@ function getBasis(unitsPerAxe) {
   const semiAxisUnits = unitsPerAxe / 2;
   const axes = {
     primitive: WebGLRenderingContext.LINES,
+    color: [0.30, 0.132, 0.73, 1],
     components: [
       -semiAxisUnits, 0,
       semiAxisUnits, 0,
@@ -281,6 +283,7 @@ function draw(components, unitsPerAxe, renderFunc) {
     getBasis(unitsPerAxe),
     {
       primitive: WebGLRenderingContext.LINE_STRIP,
+      color: [0.86, 0.101, 0.115, 1]
       components
     }
   ]);
