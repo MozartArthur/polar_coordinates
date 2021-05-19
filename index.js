96ab5be9ff6a5d8b582e9e021daf270ca303cf2a
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-19 04:28:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ function draw(components, unitsPerAxe, renderFunc) {
     getBasis(unitsPerAxe),
     {
       primitive: WebGLRenderingContext.LINE_STRIP,
-      color: [1, 1, 1, 1],
+      color: [0, 1, 1, 1],
       components
     }
   ]);
