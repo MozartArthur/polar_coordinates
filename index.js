7bf04afd2fd791208a308781a3c198071f09a6cc
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2021-05-19 04:17:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ function getBasis(unitsPerAxe) {
   const semiAxisUnits = unitsPerAxe / 2;
   const axes = {
     primitive: WebGLRenderingContext.LINES,
-    color: [0.5, 0.5, 0.5, 0],
+    color: [0.1, 0.2, 0.3, 1],
     components: [
       -semiAxisUnits, 0,
       semiAxisUnits, 0,
